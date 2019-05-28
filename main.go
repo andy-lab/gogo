06e352d1a152b563bb6a8f250ad9d05d6e603470
AndyLab <dooostyle@gmail.com>
2019-05-28 10:15:25 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,20 +12,20 @@ import (
 func main() {
   act := flag.String("cmd", "", "command name")
   opt := flag.String("opt", "", "option")
-  if act == "get" && strings.Contains(opt, "github.com") {
-    parts := strings.Split(opt, "/")
+  if *act == "get" && strings.Contains(*opt, "github.com") {
+    parts := strings.Split(*opt, "/")
     dirs := strings.Join(parts[:len(parts-1)], "/")
     fmt.Println("Creating ", dirs)
     os.MkdirAll(dirs)
-    cmd := exec.Command("git clone https://www."+opt)
+    cmd := exec.Command("git clone https://www."+*opt)
     var out bytes.Buffer
     cmd.Stdout = &out
-    err = cmd.Start()
+    err := cmd.Start()
     if err != nil {
       log.Fatal(err)
     }
     log.Println(cmd.Args)
-    err = cmd.Wait()
+    err := cmd.Wait()
     if err != nil {
       log.Printf("Command finished with error: %v", err)
     }
