05e0f2d603805c0f2583faca0c6ba4f45274cf18
AndyLab <dooostyle@gmail.com>
2019-05-28 11:47:14 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,24 +2,31 @@ package main
 
 import (
   "os"
+  "os/user"
   "bytes"
   "fmt"
   "strings"
   "flag"
   "log"
   "os/exec"
+  "path/filepath"
 )
 
 func main() {
   act := flag.String("cmd", "", "command name")
   opt := flag.String("opt", "", "option")
   flag.Parse()
+  usr, err := user.Current()
+  if err != nil {
+    fmt.Println("Get current user failed:", err)
+  }
+  gopath := filepath.Join(usr.HomeDir, "go/src/")
   if *act == "get" && strings.Contains(*opt, "github.com") {
     parts := strings.Split(*opt, "/")
     dirs := strings.Join(parts[:len(parts)-1], "/")
-    fmt.Println("Creating ", "~/go/src/"+dirs)
+    fmt.Println("Creating ", gopath+dirs)
     os.MkdirAll(dirs, 775)
-    cmd := exec.Command("git", "clone", "https://www."+*opt, "~/go/src/"+dirs)
+    cmd := exec.Command("git", "clone", "https://www."+*opt, gopath+dirs)
     var out bytes.Buffer
     cmd.Stdout = &out
     err := cmd.Start()
