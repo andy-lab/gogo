67b82312a8284c9b54b0237f3937c657eaea9263
AndyLab <dooostyle@gmail.com>
2019-05-28 11:37:34 +0800

    Update main.go

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ import (
 func main() {
   act := flag.String("cmd", "", "command name")
   opt := flag.String("opt", "", "option")
-  fmt.Println(*act, *opt)
+  flag.Parse()
   if *act == "get" && strings.Contains(*opt, "github.com") {
     parts := strings.Split(*opt, "/")
     dirs := strings.Join(parts[:len(parts)-1], "/")
-    fmt.Println("Creating ", dirs)
+    fmt.Println("Creating ", "~/go/src/"+dirs)
     os.MkdirAll(dirs, 775)
-    cmd := exec.Command("git clone https://www."+*opt)
+    cmd := exec.Command("/usr/bin/git clone https://www."+*opt)
     var out bytes.Buffer
     cmd.Stdout = &out
     err := cmd.Start()
